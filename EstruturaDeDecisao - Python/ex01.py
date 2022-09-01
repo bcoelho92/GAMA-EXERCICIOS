57320a0098bc3e1c9797ba814e9c8d301d3c5af1
Bruno Coelho <brunoc510@hotmail.com>
2022-09-01 18:00:21 -0300

    Feito

---
+++ EstruturaDeDecisao - Python/ex01.py
@@ -0,0 +1,13 @@
+# Faça um Programa que peça dois números e imprima o maior deles.
+
+print ('='*20)
+print ('='*20)
+print ('='*20)
+
+numero1 = float(input('digite o primeiro numero: '))
+numero2 = float(input('digite o segundo numero: '))
+
+if numero1 > numero2:
+     print ('O primeoro numero',numero1,'é maior que o segundo nuemro.')
+
+else:print ('O segundo numero',numero2, 'é maior que o primeiro numero!')
