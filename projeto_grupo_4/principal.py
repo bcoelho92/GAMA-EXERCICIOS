3f8c22596fdd192cc6304e67062021ce8a2654e0
Bruno Coelho <brunoc510@hotmail.com>
2022-09-08 14:51:40 -0300

    Projeto_gama_g4

---
+++ projeto_grupo_4/principal.py
@@ -0,0 +1,32 @@
+# boas vindas
+
+# Menu 
+#Digite a opção desejada:
+#
+#1 - 
+#2 - 
+#3 - 
+#4 - 
+
+
+# Variaveis 
+
+
+
+# 1 - Cadastro 
+
+
+# 2 - Vendas
+
+# 3 - Relatorio
+
+# 4 - Sair
+
+
+
+
+
+
+
+
+
