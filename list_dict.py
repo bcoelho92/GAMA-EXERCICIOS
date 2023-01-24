a6f0e24c59199c4ea56596151412df2700c63f74
Bruno Coelho <110316306+bcoelho92@users.noreply.github.com>
2023-01-24 15:56:10 -0300

    Estudo dict

---
+++ list_dict.py
@@ -0,0 +1,48 @@
+bruno = {
+    "nome":"bruno",
+    "idade":30,
+    "atuacao":"dev",
+}
+bia = {
+    "nome":"bia",
+    "idade":24,
+    "atuacao":"dev",
+}
+
+
+brunolist = ["nome","bruno","idade","bia",30,"atuacao","dev","nome","idade",24,"atuacao","dev"]
+
+list_dict = [bruno,bia]
+
+# print("BRUNO LIST")
+# for value in brunolist:
+#     print(value)
+    
+# print("LISTA DICIONARIO")
+# for dict1 in list_dict:{
+#     "nome":"bruno",
+#     "idade":30,
+#     "atuacao":"dev",
+# }
+#     print(dict1)
+
+contatos1 = {"contatos": [
+        {
+        "nome":"bruno",
+        "idade":30,
+        "atuacao":"dev",
+        },
+        {
+        "nome":"bia",
+        "idade":24,
+        "atuacao":"dev",
+        }
+    ]
+}
+
+contatos2 = {"contatos":bruno}
+
+print("contatos 1")
+print(contatos1)
+print("contatos 2")
+print(contatos2)
